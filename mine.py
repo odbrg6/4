a102df1ed8675b6f40b1a9d2321e0df3fb3f7fe3
odbrg6 <99286663+odbrg6@users.noreply.github.com>
2024-02-17 01:18:36 +0300

    Update mine.py

--- mine.py
+++ mine.py
@@ -6,12 +6,12 @@ bot = telebot.TeleBot('6724095206:AAGeobKqBMfSC_o72mbowIFm1OLlBC-_nO4')
 # Replace 'YOUR_DEVELOPER_CHAT_ID' with the chat ID of the developer
 developer_chat_id = '-1001844054913'
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    # Check if the message is deleted
-    if message.content_type == 'text':
-        # Send a notification to the developer
+@bot.message_handler(func=lambda message: True, content_types=['delete_chat_photo', 'delete_message'])
+def handle_deleted_message(message):
+    if message.content_type == 'delete_chat_photo':
+        bot.send_message(developer_chat_id, f"Admin {message.from_user.username} deleted a photo from the channel.")
+    elif message.content_type == 'delete_message':
         bot.send_message(developer_chat_id, f"Admin {message.from_user.username} deleted a message from the channel.")
-
+    
 # Start the bot
 bot.polling()
