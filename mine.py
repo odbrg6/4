8f4277462b11ff9a78ab092ccc9625fbb6ba2a6e
odbrg6 <99286663+odbrg6@users.noreply.github.com>
2024-02-17 01:23:43 +0300

    Update mine.py

--- mine.py
+++ mine.py
@@ -8,10 +8,11 @@ developer_chat_id = '-1001844054913'
 
 @bot.message_handler(func=lambda message: True, content_types=['delete_chat_photo', 'delete_message'])
 def handle_deleted_message(message):
-    if message.content_type == 'delete_chat_photo':
-        bot.send_message(developer_chat_id, f"Admin {message.from_user.username} deleted a photo from the channel.")
-    elif message.content_type == 'delete_message':
-        bot.send_message(developer_chat_id, f"Admin {message.from_user.username} deleted a message from the channel.")
+    if message.chat.type == 'channel':
+        if message.content_type == 'delete_chat_photo':
+            bot.send_message(developer_chat_id, f"Admin deleted a photo from the channel.")
+        elif message.content_type == 'delete_message':
+            bot.send_message(developer_chat_id, f"Admin deleted a message from the channel.")
     
 # Start the bot
 bot.polling()
