4d263b388bba569b09000fd4581cd17a9af056d7
odbrg6 <99286663+odbrg6@users.noreply.github.com>
2024-03-02 15:58:44 +0300

    Update bidu.py

--- bidu.py
+++ bidu.py
@@ -2,14 +2,16 @@ import asyncio
 from telethon import TelegramClient
 from telethon.tl.functions.messages import SendMessageRequest, DeleteMessagesRequest
 
-# تعيين المتغيرات
+# تعريف الجلسة
+session = '1BJWap1sBuzDl_dHpqdh56xRpRnd7L6BkcfS1UJjWAl9uCX8oGuRO0P727UZoK55c-vj_8GRle90HQ4boWB_FL6a7bhiI9mSBYnBcAygyHvn49mZyF8NwlQNLzT-SOqw80Svc4zz1xj56G2mkMNZWIvPHIv9SRLN1620GM6A8DpjmYAHSJpxr7OlM66qRcPzgDeEXOKzPfZkBJOX-jrSmTyO1LQeJu4N1dgYjMZs4_ScSG7Rv-0gWQ1byUP7AuO6MniLyo6uFmMIQZUGnj5BT6HLmkeSXOKGL0yK0CflL8lfK5ZnYpuBSZ1bT-1_Wj50LpWaVzx1a-rDTG68IyRi92hYiLCacMdg='
+
+# تعريف بيانات الوصول
 api_id = '22877012'
 api_hash = '6d59bf3287c22f491e291a8a366c997c'
-phone_number = '3584573989485'
 
-# بدء الاتصال بالتليجرام
-client = TelegramClient('session_name', api_id, api_hash)
-client.start(phone_number)
+# بدء الاتصال بالتليجرام باستخدام الجلسة
+client = TelegramClient(session, api_id, api_hash)
+client.start()
 
 # دالة لإرسال الرسالة وحذفها بعد فترة زمنية محددة
 async def send_and_delete(message, delay):
