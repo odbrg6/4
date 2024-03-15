493643642cdccce0970d871bdcc50a3db12b1b16
odbrg6 <99286663+odbrg6@users.noreply.github.com>
2024-03-16 02:46:46 +0300

    Update bidu.py

--- bidu.py
+++ bidu.py
@@ -1,34 +1,24 @@
-import asyncio
-from telethon import TelegramClient
-from telethon.tl.functions.messages import SendMessageRequest, DeleteMessagesRequest
-
-# تعريف الجلسة
-session = '1BJWap1sBuzDl_dHpqdh56xRpRnd7L6BkcfS1UJjWAl9uCX8oGuRO0P727UZoK55c-vj_8GRle90HQ4boWB_FL6a7bhiI9mSBYnBcAygyHvn49mZyF8NwlQNLzT-SOqw80Svc4zz1xj56G2mkMNZWIvPHIv9SRLN1620GM6A8DpjmYAHSJpxr7OlM66qRcPzgDeEXOKzPfZkBJOX-jrSmTyO1LQeJu4N1dgYjMZs4_ScSG7Rv-0gWQ1byUP7AuO6MniLyo6uFmMIQZUGnj5BT6HLmkeSXOKGL0yK0CflL8lfK5ZnYpuBSZ1bT-1_Wj50LpWaVzx1a-rDTG68IyRi92hYiLCacMdg='
-
-# تعريف بيانات الوصول
-api_id = '22877012'
-api_hash = '6d59bf3287c22f491e291a8a366c997c'
-
-# بدء الاتصال بالتليجرام باستخدام الجلسة
-client = TelegramClient(session, api_id, api_hash)
-client.start()
-
-# دالة لإرسال الرسالة وحذفها بعد فترة زمنية محددة
-async def send_and_delete(message, delay):
-    # إرسال الرسالة
-    sent_message = await client(SendMessageRequest('-1002106912636', message))
-    # انتظار فترة زمنية محددة
-    await asyncio.sleep(delay)
-    # حذف الرسالة
-    await client(DeleteMessagesRequest('-1002106912636', [sent_message.id]))
-
-async def main():
-    # إرسال الرسالة رقم 1
-    await send_and_delete('ارسال 1', 40)
-    # انتظار ثانيتين
-    await asyncio.sleep(2)
-    # إرسال الرسالة رقم 2
-    await send_and_delete('ارسال 2', 40)
-
-# تشغيل البرنامج الرئيسي
-client.start()
+import telebot , requests , json ; from telebot import types
+bot = telebot.TeleBot(input('- Enter Token : '))
+@bot.message_handler(commands=['start'])
+def start(message):
+	start = types.InlineKeyboardButton(text='- تنزيل فيديو',callback_data='start')
+	Ronaldo = types.InlineKeyboardMarkup(row_width=2) ; Ronaldo.add(start)
+	bot.send_message(message.chat.id,text='- اهلا بك عزيزي في بوت تحميل من تيك توك اختر من الازرار التاليه',reply_markup=Ronaldo)
+@bot.callback_query_handler(func=lambda call:True)
+def start2(call):
+	if call.data=='start':
+		ji = bot.send_message(call.message.chat.id,text='- اهلاً بَك عزيزي ارسل الرابط الان .')
+		bot.register_next_step_handler(ji,dow)
+def dow(message):
+		url = message.text
+		headers = {"referer":"https://lovetik.com/sa/video/","origin":"https://lovetik.com","user-agent":"Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0"}
+		payload = {"query":url}
+		url =requests.post("https://lovetik.com/api/ajax/search",headers = headers,data=payload).json()
+		try:
+			respones=json.loads('{"ok":"true","Video":{"authorUser":"'+url['author']+'","authorName":"'+url['author_name']+'","authorImage":"'+url['author_a']+'","cover":"'+url['cover']+'","vidID":"'+url['vid']+'","desc":"'+url["desc"]+'","link":"'+url['links'][4]['a']+'","audioName":"'+url['links'][5]['s']+'","audioLink":"'+url['links'][5]['a']+'"}}') 
+		except:
+			bot.send_message(message.chat.id,'- عذراً عزيزي الرابط غير صالح !')
+		bot.send_video(message.chat.id,respones['Video']['link'],caption='- Done Download Video .')
+if __name__=="__main__":
+	bot.infinity_polling()
